59f76b1c822bed29da67525d91f17fac0331198a
kkay19 <supermariomario184four@gmail.com>
2024-05-27 17:53:44 +0700

    BIG CHANGE

---
+++ scrolltotop.js
@@ -0,0 +1,16 @@
+function scrollToTop() {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+}
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("back-to-top").style.display = "block";
+    } else {
+        document.getElementById("back-to-top").style.display = "none";
+    }
+}
